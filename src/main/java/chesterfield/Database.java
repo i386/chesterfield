4c74250594f3f203b8160fc5f6e429bbc87e8f73
James William Dumay <james.w.dumay@gmail.com>
2009-08-02 22:17:33 +1000

    Able to fetch documents from the database as JsonElements or pick a class type to deserialize from :-)

--- src/main/java/chesterfield/Database.java
+++ src/main/java/chesterfield/Database.java
@@ -1,6 +1,8 @@
 package chesterfield;
 
 import com.google.gson.JsonObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 import java.net.URLEncoder;
 import java.io.UnsupportedEncodingException;
@@ -40,19 +42,51 @@ public class Database
     }
 
     /**
+     * Get the couch client
+     * @return client
+     */
+    CouchClient getClient()
+    {
+        return getSession().getClient();
+    }
+
+    /**
      * Get the Database Url
      * @return dbUrl
      */
     String getDbUrl()
     {
-        try
-        {
-            return session.getBaseUrl() + URLEncoder.encode(name, "UTF-8") + "/";
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            throw new RuntimeException(e.getMessage(), e);
-        }
+        return session.getBaseUrl() + urlEncode(name) + "/";
+    }
+
+    /**
+     * Get the document with the given id
+     * @param id
+     * @return element
+     */
+    public JsonObject getDocument(String id)
+    {
+        return (JsonObject)getDocumentAsJsonElement(id);
+    }
+
+    private JsonElement getDocumentAsJsonElement(String id)
+    {
+        final CouchResult<JsonElement> result = getClient().createRequest(getDbUrl() + urlEncode(id)).execute(HttpMethod.GET);
+        return result.isOK() ? result.getElement() : null;
+    }
+
+    /**
+     * Get Document with the given id
+     * @param id of the document
+     * @param <T> type of the object to map to
+     * @return document
+     */
+    public <T> T getDocument(String id, Class<T> t)
+    {
+        final JsonElement element = getDocumentAsJsonElement(id);
+        if (element == null) return null;
+        Gson gson = new Gson();
+        return gson.fromJson(element, t);
     }
 
     /**
@@ -61,7 +95,19 @@ public class Database
      */
     public int getDocumentCount()
     {
-        final CouchResult<JsonObject> result = session.getClient().createRequest(getDbUrl()).execute(HttpMethod.GET);
+        final CouchResult<JsonObject> result = getClient().createRequest(getDbUrl()).execute(HttpMethod.GET);
         return result.getElement().get(DOC_COUNT).getAsInt();
+    }
+
+    private String urlEncode(String s)
+    {
+        try
+        {
+            return URLEncoder.encode(s, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 }
