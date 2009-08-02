24a03b4ebb2a8379f38d988c7022996932855ad5
James William Dumay <james.w.dumay@gmail.com>
2009-08-02 21:52:34 +1000

    Add javadoc for CouchRequest

--- src/main/java/chesterfield/CouchRequest.java
+++ src/main/java/chesterfield/CouchRequest.java
@@ -2,9 +2,26 @@ package chesterfield;
 
 import com.google.gson.JsonElement;
 
+/**
+ * Represents a request that can be executed by a {@link chesterfield.CouchClient}
+ * @param <T> JsonElement that is returned from the response body
+ */
 public interface CouchRequest<T extends JsonElement>
 {
+    /**
+     * Executes the request
+     * @param method
+     * @return result
+     * @throws WireException
+     */
     CouchResult<T> execute(HttpMethod method) throws WireException;
 
+    /**
+     * Executes the request with a request body
+     * @param method
+     * @param body
+     * @return result
+     * @throws WireException
+     */
     CouchResult<T> executeWithBody(HttpMethod method, String body) throws WireException;
 }
